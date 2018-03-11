1650238d766e86dacfe3bad22d45563795828d3d
Vinícius do Carmo <vinicius.m.do.carmo@gmail.com>
2018-03-11 19:58:10 -0300

    Implement simple GUI interface

--- main.go
+++ main.go
@@ -1,50 +1,61 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
 	"net"
 	"os"
-
-	"./socket"
+	"strings"
 )
 
+type gui struct {
+}
+
+func (g gui) init() {
+	fmt.Println(`
+   _____               __          __ 
+  / ___/ ____   _____ / /__ ___   / /_
+  \__ \ / __ \ / ___// //_// _ \ / __/
+ ___/ // /_/ // /__ / ,<  /  __// /_  
+/____/ \____/ \___//_/|_| \___/ \__/  
+												
+		  `)
+}
+
 func main() {
-	s := socket.NewScoket()
+	// s := socket.NewScoket()
+	g := gui{}
 
-	host1, port1, err := net.SplitHostPort(os.Args[1])
-
-	if err != nil {
-		log.Fatal("Error reading address from command line arguments")
-		os.Exit(1)
-	}
-
-	host2, port2, err := net.SplitHostPort(os.Args[2])
+	serverHost, serverPort, err := net.SplitHostPort(os.Args[1])
 
 	if err != nil {
 		log.Fatal("Error reading address from command line arguments")
 		os.Exit(1)
 	}
 
-	cServer := make(chan int)
+	peerHost, peerPort, err := net.SplitHostPort(os.Args[2])
 
-	go func() {
-		err := s.Listen(host1, port1)
+	if err != nil {
+		log.Fatal("Error reading address from command line arguments")
+		os.Exit(1)
+	}
 
-		if err != nil {
-			log.Fatal("Error: ", err)
-			os.Exit(1)
+	g.init()
+	fmt.Printf("👂 socket listening on \t%s\n", net.JoinHostPort(serverHost, serverPort))
+	fmt.Printf("🖥️ connected to peer \t%s\n\n\n", net.JoinHostPort(peerHost, peerPort))
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Printf("💬 %s: ", net.JoinHostPort(serverHost, serverPort))
+		text, _ := reader.ReadString('\n')
+		// convert CRLF to LF
+		text = strings.Replace(text, "\n", "", -1)
+
+		if strings.Compare("hi", text) == 0 {
+			fmt.Printf("📥 %s: \n", net.JoinHostPort(peerHost, peerPort))
 		}
+	}
 
-		cServer <- 0
-	}()
-
-	cClient := make(chan int)
-
-	go func() {
-		s.Ping(host2, port2)
-		cClient <- 0
-	}()
-
-	<-cServer
-	<-cClient
 }
